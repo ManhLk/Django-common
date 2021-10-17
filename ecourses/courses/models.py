49bcdcd65268c62271e0fe1180fc0ab451e947c6
huyphuong99 <phuong.nguyenhuy.hust@gmail.com>
2021-10-17 23:17:22 +0700

    update lession from 1 to 5

--- ecourses/courses/models.py
+++ ecourses/courses/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    avatar = models.ImageField(upload_to="uploads/%Y/%m")
+
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, null=False, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ItemBase(models.Model):
+    subjects = models.CharField(max_length=255, null=False)
+    image = models.ImageField(upload_to="upload_to/%Y/%m", default=None)
+    create_date  = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
+
+
+
+class Course(ItemBase):
+    description = models.TextField(null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.subjects, self.description, self.category
+
+    class Meta:
+        unique_together = ("subjects", "category")
+
+
+
+class Lesson(ItemBase):
+    image = models.ImageField(upload_to="courses/%Y/%m")
+    active  = models.BooleanField(default=True)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ("subjects", "course")
--- ecourses/ecourses/settings.py
+++ ecourses/ecourses/settings.py
@@ -77,12 +77,15 @@ WSGI_APPLICATION = 'ecourses.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "db_courses",
+        "USER": "admin",
+        "PASSWORD": "Phuong99@123",
+        "HOST": ""
     }
 }
 
-
+AUTH_USER_MODEL = "courses.User"
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
