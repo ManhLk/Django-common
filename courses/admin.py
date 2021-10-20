f0dece5ffc1545e6608930d12c880ac44130cf81
superman <khacmanhsss99@gmail.com>
2021-10-20 16:00:49 +0700

    Create InlineModelAdmin of Category, Course, Lesson and Tag in courses/admin.py

--- courses/admin.py
+++ courses/admin.py
@@ -8,10 +8,21 @@ from ckeditor_uploader.widgets import CKEditorUploadingWidget
 
 # Register your models here.
 
+class CourseInline(admin.StackedInline):
+    model = Course
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = (CourseInline,)
+
+
+class LessonInline(admin.StackedInline):
+    model = Lesson
+
 class CourseAdmin(admin.ModelAdmin):
     list_display = ["subject", "created_date", "active", "category"]
     list_filter = ["active", "category__name"]
     search_fields = ["subject", "category__name"]
+    inlines = (LessonInline,)
     # Display image
     readonly_fields = ["image_course"]
     def image_course(self, course):
@@ -22,6 +33,7 @@ class CourseAdmin(admin.ModelAdmin):
             'all': ('/static/css/main.css',)
         }
 
+
 class LessonForm(forms.ModelForm):
     content = forms.CharField(widget= CKEditorUploadingWidget)
     
@@ -29,12 +41,16 @@ class LessonForm(forms.ModelForm):
         model = Lesson
         fields = '__all__'
 
+class LessonTagInline(admin.TabularInline):
+    model = Lesson.tags.through
+
 
 class LessonAdmin(admin.ModelAdmin):
     form = LessonForm
     list_display = ["subject", "created_date", "updated_date", "active", "course"]
     list_filter = ["active", "subject"]
     search_fields = ["subject", "course__name"]
+    inlines = (LessonTagInline,)
 
     # Dispaly image
     readonly_fields = ["lesson_image"]
@@ -42,6 +58,11 @@ class LessonAdmin(admin.ModelAdmin):
         if lesson:
             return mark_safe(f'<image src="/static/image/{lesson.image.name}" alt="{lesson.subject}" style="width:180px; height:128px;">')
 
-admin.site.register(Category)
+class TagAdmin(admin.ModelAdmin):
+    inlines = (LessonTagInline,)
+    list_display = ['name',]
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Tag, TagAdmin)
